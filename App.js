b04c106f699db15f8b903fe502b4b9b32bbe34f7
mjtoolbox <michael.jo@gmail.com>
2020-11-07 12:47:15 -0800

    Main, Mood screens added

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const loadFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+    
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
---
+++ screens/MoodMainScreen.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const MoodMainScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default MoodMainScreen;
