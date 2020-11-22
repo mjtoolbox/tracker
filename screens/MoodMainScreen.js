b23ca3b2e1259783d3743257411709cda643ff1f
mjtoolbox <michael.jo@gmail.com>
2020-11-22 11:59:39 -0800

    Plus icon moved to right

--- screens/MoodMainScreen.js
+++ screens/MoodMainScreen.js
@@ -7,12 +7,13 @@ import MoodList from '../components/MoodList';
 const MoodMainScreen = (props) => {
   return (
     <View style={styles.container}>
-      <Icon
-        name='add'
-        raised
-        style={styles.iconContainer}
-        onPress={() => props.navigation.navigate('NewMood')}
-      />
+      <View style={styles.iconContainer}>
+        <Icon
+          name='add'
+          raised
+          onPress={() => props.navigation.navigate('NewMood')}
+        />
+      </View>
       <MoodStatus />
       <MoodList />
     </View>
@@ -23,15 +24,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
-    // alignItems: 'stretch',
-    // justifyContent: 'flex-start',
   },
   iconContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    alignItems: 'flex-end',
+    padding: 5,
   },
 });
 export default MoodMainScreen;
